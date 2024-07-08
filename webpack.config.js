deea0889c38749064fdae31e35bb8d2284ded502
eonduck2 <whdtnxd@gmail.com>
2024-07-08 13:15:29 +0900

    :tada: 프로젝트 초기화

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("node:path");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "app.tsx"),
+  output: {
+    filename: "app.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["js", ".ts", "jsx", "tsx", "css"],
+  },
+  devtool: "source-map",
+};
